215ab86727133c66f245c26eb3c5b592a0fce9b4
Vikas Suresh <vsvikassvs@gmail.com>
2019-09-28 19:40:46 +0530

    Added code to read build folder from client

--- server/server.js
+++ server/server.js
@@ -7,6 +7,16 @@ require('./startup/db')();
 require('./startup/prod')(app);
 require('./startup/route')(app);
 
+
+
+if(process.env.NODE_ENV==='production'){
+    app.use(express.static('twiapp/build'));
+    
+    app.get('*',(req,res)=>{
+        res.sendFile(path.resolve(__dirname,'twiapp','build','index.html'));
+    })
+}
+
 const port = process.env.PORT || 1000;
 
 app.listen(port,()=>{    
