8f52f315f9d957aa2f0185842b611c9b66c7bd2d
Vikas Suresh <vsvikassvs@gmail.com>
2019-10-17 21:34:30 +0530

    removed static build

--- server/server.js
+++ server/server.js
@@ -36,14 +36,6 @@ app.get('/sign-s3', (req, res) => {
     });
   });
 
-if(process.env.NODE_ENV==='production'){
-    app.use(express.static('twiapp/build'));
-    
-    app.get('*',(req,res)=>{
-        res.sendFile(path.resolve(__dirname,'twiapp','build','index.html'));
-    })
-}
-
 const port = process.env.PORT || 1000;
 
 app.listen(port,()=>{    
